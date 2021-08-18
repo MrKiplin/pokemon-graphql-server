f2649ffce423d8e83578b1c447ba11a7a486bcb2
MrKiplin <tjworks@hotmail.co.uk>
2021-08-18 01:52:22 +0100

    hooked up useQuery

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { Button, Container, Divider, Header, Input } from "semantic-ui-react";
-import { useForm } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  Container,
+  Form,
+  Header,
+  Input,
+} from "semantic-ui-react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { PokemonQuery, usePokemonQuery } from "../__generated__/graphql";
 import MessageAlertError from "../components/MessageError";
 import PokemonTable from "../components/PokemonTable";
+
+interface FormInput {
+  pokemonNameOrId: string;
+}
 
 interface HomeProps {
   title: string;
@@ -13,15 +24,20 @@ interface HomeProps {
 export const Home: React.FC<HomeProps> = ({ title }) => {
   const [errorMessage, setErrorMessage] = useState("");
   const [pokemon, setPokemon] = useState<PokemonQuery | undefined>(undefined);
-  const { handleSubmit } = useForm();
+  const [formState, setFormState] = useState<FormInput>({
+    pokemonNameOrId: "",
+  });
+  const { handleSubmit, control } = useForm<FormInput>();
 
   const { data, loading, error } = usePokemonQuery({
     variables: {
-      pokemonNameOrId: "charizard",
+      pokemonNameOrId: formState?.pokemonNameOrId,
     },
   });
 
-  const onSubmit = async (): Promise<void> => {
+  const onSubmit: SubmitHandler<FormInput> = (formData) => {
+    setFormState(formData);
+
     if (error) {
       setErrorMessage(`There was a problem with the server: ${error.message}`);
     }
@@ -45,15 +61,25 @@ export const Home: React.FC<HomeProps> = ({ title }) => {
 
         {errorMessage ? <MessageAlertError message={errorMessage} /> : null}
 
-        <Divider horizontal>Search</Divider>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Field>
+            <label>Pokemon Name or ID</label>
+            <Controller
+              name="pokemonNameOrId"
+              control={control}
+              render={({ field }) => (
+                <Input placeholder="Search..." {...field} />
+              )}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label="I am not a robot" />
+          </Form.Field>
+          <Button type="submit" disabled={loading}>
+            Submit
+          </Button>
+        </Form>
 
-        <Input name="pokemon" id="pokemon" placeholder="Search..." />
-
-        <Button
-          content="Submit"
-          disabled={loading}
-          onClick={handleSubmit(onSubmit)}
-        />
         {pokemon ? <PokemonTable pokemonQuery={pokemon} /> : null}
       </Container>
     </>

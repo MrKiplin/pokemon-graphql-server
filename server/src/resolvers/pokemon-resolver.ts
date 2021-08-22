115b8ac50d72e61b9ce1bc907cc11738cba3601a
MrKiplin <tjworks@hotmail.co.uk>
2021-08-22 04:49:21 +0100

    Move Pokemon service to constructer

--- server/src/resolvers/pokemon-resolver.ts
+++ server/src/resolvers/pokemon-resolver.ts
@@ -3,16 +3,19 @@ import { Pokemon, PokemonService } from '../pokemon-service/pokemon-service';
 import { PokemonGQL } from './pokemon-type';
 import axios from 'axios';
 
-// TODO: Move pokemon service to constructor, https://www.npmjs.com/package/typedi looks interesting for handling dependency injection
 // TODO: Add unit tests
-const pokemonService = new PokemonService(axios.create({ baseURL: 'https://pokeapi.co/api/v2' }));
-
 @Resolver()
 export class PokemonResolver {
+  constructor(private pokemonService: PokemonService) {
+    this.pokemonService = new PokemonService(
+      axios.create({ baseURL: 'https://pokeapi.co/api/v2' }),
+    );
+  }
+
   @Query(() => PokemonGQL)
   async pokemon(
     @Arg('pokemonNameOrId', () => ID) pokemonNameOrId: number | string,
   ): Promise<Pokemon> {
-    return await pokemonService.getPokemonInfo(pokemonNameOrId);
+    return await this.pokemonService.getPokemonInfo(pokemonNameOrId);
   }
 }
